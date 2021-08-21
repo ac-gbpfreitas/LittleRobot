b824ebdd18517db4f0295b02813ad99390afcc44
Gustavo Freitas <70547481+ac-gbpfreitas@users.noreply.github.com>
2021-08-21 11:40:02 -0700

    C# small robot simulation

---
+++ Lab10-Robot/Lab10-Robot/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace Lab10_Robot
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelPosition = new System.Windows.Forms.Label();
+            this.btnGo1 = new System.Windows.Forms.Button();
+            this.btnWest = new System.Windows.Forms.Button();
+            this.btnNorth = new System.Windows.Forms.Button();
+            this.btnSouth = new System.Windows.Forms.Button();
+            this.btnEast = new System.Windows.Forms.Button();
+            this.btnGo10 = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panelArea = new System.Windows.Forms.Panel();
+            this.robot = new System.Windows.Forms.Label();
+            this.panelArea.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelPosition
+            // 
+            this.labelPosition.AutoSize = true;
+            this.labelPosition.Location = new System.Drawing.Point(29, 9);
+            this.labelPosition.Name = "labelPosition";
+            this.labelPosition.Size = new System.Drawing.Size(38, 15);
+            this.labelPosition.TabIndex = 0;
+            this.labelPosition.Text = "label1";
+            // 
+            // btnGo1
+            // 
+            this.btnGo1.Location = new System.Drawing.Point(52, 287);
+            this.btnGo1.Name = "btnGo1";
+            this.btnGo1.Size = new System.Drawing.Size(48, 23);
+            this.btnGo1.TabIndex = 2;
+            this.btnGo1.Text = "Go 1";
+            this.btnGo1.UseVisualStyleBackColor = true;
+            this.btnGo1.Click += new System.EventHandler(this.btnGo1_Click);
+            // 
+            // btnWest
+            // 
+            this.btnWest.Location = new System.Drawing.Point(107, 287);
+            this.btnWest.Name = "btnWest";
+            this.btnWest.Size = new System.Drawing.Size(23, 22);
+            this.btnWest.TabIndex = 3;
+            this.btnWest.Text = "W";
+            this.btnWest.UseVisualStyleBackColor = true;
+            this.btnWest.Click += new System.EventHandler(this.btnWest_Click);
+            // 
+            // btnNorth
+            // 
+            this.btnNorth.Location = new System.Drawing.Point(127, 269);
+            this.btnNorth.Name = "btnNorth";
+            this.btnNorth.Size = new System.Drawing.Size(23, 22);
+            this.btnNorth.TabIndex = 4;
+            this.btnNorth.Text = "N";
+            this.btnNorth.UseVisualStyleBackColor = true;
+            this.btnNorth.Click += new System.EventHandler(this.btnNorth_Click);
+            // 
+            // btnSouth
+            // 
+            this.btnSouth.Location = new System.Drawing.Point(127, 306);
+            this.btnSouth.Name = "btnSouth";
+            this.btnSouth.Size = new System.Drawing.Size(23, 22);
+            this.btnSouth.TabIndex = 5;
+            this.btnSouth.Text = "S";
+            this.btnSouth.UseVisualStyleBackColor = true;
+            this.btnSouth.Click += new System.EventHandler(this.btnSouth_Click);
+            // 
+            // btnEast
+            // 
+            this.btnEast.Location = new System.Drawing.Point(147, 287);
+            this.btnEast.Name = "btnEast";
+            this.btnEast.Size = new System.Drawing.Size(23, 22);
+            this.btnEast.TabIndex = 6;
+            this.btnEast.Text = "E";
+            this.btnEast.UseVisualStyleBackColor = true;
+            this.btnEast.Click += new System.EventHandler(this.btnEast_Click);
+            // 
+            // btnGo10
+            // 
+            this.btnGo10.Location = new System.Drawing.Point(176, 287);
+            this.btnGo10.Name = "btnGo10";
+            this.btnGo10.Size = new System.Drawing.Size(48, 23);
+            this.btnGo10.TabIndex = 7;
+            this.btnGo10.Text = "Go 10";
+            this.btnGo10.UseVisualStyleBackColor = true;
+            this.btnGo10.Click += new System.EventHandler(this.btnGo10_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(147, 343);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // panelArea
+            // 
+            this.panelArea.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelArea.Controls.Add(this.robot);
+            this.panelArea.Location = new System.Drawing.Point(29, 41);
+            this.panelArea.Name = "panelArea";
+            this.panelArea.Size = new System.Drawing.Size(215, 215);
+            this.panelArea.TabIndex = 9;
+            // 
+            // robot
+            // 
+            this.robot.AutoSize = true;
+            this.robot.Font = new System.Drawing.Font("Wingdings", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.robot.Location = new System.Drawing.Point(-1, 140);
+            this.robot.Name = "robot";
+            this.robot.Size = new System.Drawing.Size(20, 14);
+            this.robot.TabIndex = 0;
+            this.robot.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 385);
+            this.Controls.Add(this.panelArea);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGo10);
+            this.Controls.Add(this.btnEast);
+            this.Controls.Add(this.btnSouth);
+            this.Controls.Add(this.btnNorth);
+            this.Controls.Add(this.btnWest);
+            this.Controls.Add(this.btnGo1);
+            this.Controls.Add(this.labelPosition);
+            this.Name = "Form1";
+            this.Text = "Robot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelArea.ResumeLayout(false);
+            this.panelArea.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelPosition;
+        private System.Windows.Forms.Button btnGo1;
+        private System.Windows.Forms.Button btnWest;
+        private System.Windows.Forms.Button btnNorth;
+        private System.Windows.Forms.Button btnSouth;
+        private System.Windows.Forms.Button btnEast;
+        private System.Windows.Forms.Button btnGo10;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel panelArea;
+        private System.Windows.Forms.Label robot;
+    }
+}
+
---
+++ Lab10-Robot/Lab10-Robot/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab10_Robot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()        {
+            InitializeComponent();
+        }
+
+        private Robot currentRobot = new Robot();
+
+        private const string EAST = "";
+        private const string WEST = "";
+        private const string NORTH = "";
+        private const string SOUTH = "";
+        
+
+        private void btnExit_Click(object sender, EventArgs e)        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)        {
+            robot.Font = new Font("Wingdings",8);
+            robot.Text = NORTH;
+            robot.Location = new Point(100,100);
+            labelPosition.Text = "{X="+currentRobot.CurrentPoint.X+",Y="+ currentRobot.CurrentPoint.Y+"}";
+        }
+
+        private void btnNorth_Click(object sender, EventArgs e)        {
+            currentRobot.CurrentDirection = "N";
+            robot.Text = NORTH;
+        }
+
+        private void btnSouth_Click(object sender, EventArgs e)        {
+            currentRobot.CurrentDirection = "S";
+            robot.Text = SOUTH;
+        }
+
+        private void btnEast_Click(object sender, EventArgs e)        {
+            currentRobot.CurrentDirection = "E";
+            robot.Text = EAST;
+        }
+
+        private void btnWest_Click(object sender, EventArgs e)        {
+            currentRobot.CurrentDirection = "W";
+            robot.Text = WEST;
+        }
+
+        private void btnGo1_Click(object sender, EventArgs e) {
+
+            Point newPosition;
+            switch (currentRobot.CurrentDirection) {
+
+                case "N":
+                    newPosition = new Point(
+                        currentRobot.CurrentPoint.X,
+                        currentRobot.CurrentPoint.Y+1
+                        );
+
+                    currentRobot.moveRobot(newPosition);
+                    moveArrow(0,-1);
+                    break;
+
+                case "S":
+                    newPosition = new Point(
+                        currentRobot.CurrentPoint.X,
+                        currentRobot.CurrentPoint.Y - 1
+                        );
+
+                    currentRobot.moveRobot(newPosition);
+                    moveArrow(0,1);
+                    break;
+
+                case "W":
+                    newPosition = new Point(
+                        currentRobot.CurrentPoint.X - 1,
+                        currentRobot.CurrentPoint.Y
+                        );
+
+                    currentRobot.moveRobot(newPosition);
+
+                    moveArrow(-1,0);
+                    break;
+
+                case "E":
+                    newPosition = new Point(
+                        currentRobot.CurrentPoint.X + 1,
+                        currentRobot.CurrentPoint.Y
+                        );
+
+                    currentRobot.moveRobot(newPosition);
+
+                    moveArrow(1,0);
+                    break;
+            }
+            
+        }
+
+        private void btnGo10_Click(object sender, EventArgs e) {
+            
+            Point newPosition;
+            switch (currentRobot.CurrentDirection) {
+
+                case "N":
+                    newPosition = new Point(
+                        currentRobot.CurrentPoint.X,
+                        currentRobot.CurrentPoint.Y+10
+                        );
+
+                    currentRobot.moveRobot(newPosition);
+                    moveArrow(0,-10);
+                    break;
+
+                case "S":
+                    newPosition = new Point(
+                        currentRobot.CurrentPoint.X,
+                        currentRobot.CurrentPoint.Y - 10
+                        );
+
+                    currentRobot.moveRobot(newPosition);
+                    moveArrow(0,10);
+                    break;
+
+                case "W":
+                    newPosition = new Point(
+                        currentRobot.CurrentPoint.X - 10,
+                        currentRobot.CurrentPoint.Y
+                        );
+
+                    currentRobot.moveRobot(newPosition);
+
+                    moveArrow(-10,0);
+                    break;
+
+                case "E":
+                    newPosition = new Point(
+                        currentRobot.CurrentPoint.X + 10,
+                        currentRobot.CurrentPoint.Y
+                        );
+
+                    currentRobot.moveRobot(newPosition);
+
+                    moveArrow(10,0);
+                    break;
+            }
+            
+        }
+
+        private void moveArrow(int x, int y) {
+
+            if ((robot.Location.X+x) <= 201 && (robot.Location.X + x) >= -2) {
+                robot.Location = new Point(
+                    (robot.Location.X + x),
+                    (robot.Location.Y)
+                );
+            }
+
+            if ( (robot.Location.Y+y) <= 201 && (robot.Location.Y + y) >= -2) {
+                robot.Location = new Point(
+                    (robot.Location.X),
+                    (robot.Location.Y+y)
+                );
+            }
+
+            labelPosition.Text = "{X=" + currentRobot.CurrentPoint.X + ",Y=" + currentRobot.CurrentPoint.Y + "}";
+
+        }
+    }
+}
